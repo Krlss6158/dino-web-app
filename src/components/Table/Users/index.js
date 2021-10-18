742810d6cbaa7e1bf4ca676ca03e833afd803ec2
Carlos Pico <cpico6375@gmail.com>
2021-10-18 15:19:14 -0500

    errores de deploy

--- src/components/Table/Users/index.js
+++ src/components/Table/Users/index.js
@@ -59,11 +59,11 @@ const Table = ({
 
 
     return (
-        <div class="flex flex-col">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
+        <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-200">
 
                             <Header
                                 arr={header}
@@ -71,36 +71,36 @@ const Table = ({
                                     setSorting({ field, order })
                                 } />
 
-                            <tbody class="bg-white divide-y divide-gray-200">
+                            <tbody className="bg-white divide-y divide-gray-200">
 
                                 {commentsData.map(comment => (
                                     <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="flex items-center">
-                                                <div class="flex-shrink-0 h-10 w-10">
-                                                    <img class="h-10 w-10 rounded-full" src="https://picsum.photos/200" alt="" />
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="flex items-center">
+                                                <div className="flex-shrink-0 h-10 w-10">
+                                                    <img className="h-10 w-10 rounded-full" src="https://picsum.photos/200" alt="" />
                                                 </div>
-                                                <div class="ml-4">
-                                                    <div class="text-sm font-medium text-gray-900">
+                                                <div className="ml-4">
+                                                    <div className="text-sm font-medium text-gray-900">
                                                         {comment.name}
                                                     </div>
-                                                    <div class="text-sm text-gray-500">
+                                                    <div className="text-sm text-gray-500">
                                                         jane.cooper@example.com
                                                     </div>
                                                 </div>
                                             </div>
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                             Hace una semana
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                                                 Admin
                                             </span>
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                            <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
                                         </td>
                                     </tr>
                                 ))}
